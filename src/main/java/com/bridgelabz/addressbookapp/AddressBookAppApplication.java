96b4f767652d4d6181ee5f1f38bd1264ba802430
Sakshi <sakshishetty1231997@gmail.com>
2021-10-11 19:30:42 +0530

    Added: Dependencies required for address book app development

---
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * To implement a Address Book App development
+ * @author - Sakshi Shetty
+ * @version - 0.0.1
+ * @since - 10/10/2021
+ */
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+}
